54bb149c8390868a718f905a130858a39f675c62
Priyanshu <technicalpriyanshutaru@gmail.com>
2021-07-18 21:22:40 +0530

    This is Basic Commit FOr program 2

---
+++ 2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    //int number;
+    //cout<<"Enter the number;"<<endl;
+    //cin>> number;
+
+    //cout<<"your Entered number = " <<number;
+    //int number;
+
+    //cout << "Enter an integer: ";
+    //cin >> number;
+
+    //cout << "You entered " << number;    
+
+    int t;
+    cin>>t;
+    cout<<t;
+    
+
+    return 0;
+}
